363624cc415f8372a8bf42e53d0e48ebcf05489f
khdream <truedev1001@gmail.com>
2023-11-27 23:31:44 -0500

    Data Ingestion stage completed

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "Audio-Based-Anomaly-Detection"
+project_name = "Anomaly_Detection"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
